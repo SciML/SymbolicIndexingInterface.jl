b13b45ebc303360b52dbe751644d3359711bd853
xtalax <alex.lemony@gmail.com>
2023-01-27 15:55:22 +0000

    default tests

--- test/default_function_test.jl
+++ test/default_function_test.jl
@@ -3,8 +3,11 @@ using SymbolicIndexingInterface, Test
 @test independent_variables(nothing) == []
 @test states(nothing) == []
 @test parameters(nothing) == []
+@test observed(nothing) == []
 @test !is_indep_sym(nothing, :a)
 @test !is_state_sym(nothing, :a)
 @test !is_param_sym(nothing, :a)
+@test !is_observed_sym(nothing, :a)
 @test isnothing(state_sym_to_index(nothing, :a))
 @test isnothing(param_sym_to_index(nothing, :a))
+@test get_state_dependencies(nothing) = []
