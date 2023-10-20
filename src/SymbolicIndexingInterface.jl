0e6e9e794a261101abe5d6b2eff54bbf59244e2c
Aayush Sabharwal <aayush.sabharwal@juliahub.com>
2023-10-20 13:19:16 +0530

    feat!: rework interface

--- src/SymbolicIndexingInterface.jl
+++ src/SymbolicIndexingInterface.jl
@@ -1,13 +1,72 @@
 module SymbolicIndexingInterface
 
-using DocStringExtensions
+export isvariable, variableindex, isparameter, parameterindex, isindependent_variable,
+    isobserved, observed, istimedependent, constant_structure
 
-include("interface.jl")
-include("symbolcache.jl")
+"""
+    isvariable(sys, sym)
 
-export independent_variables, is_indep_sym, states, state_sym_to_index, is_state_sym,
-       parameters, param_sym_to_index, is_param_sym, observed, observed_sym_to_index,
-       is_observed_sym, get_state_dependencies, get_observed_dependencies,
-       get_deps_of_observed, SymbolCache, unknown_states
+Check whether the given `sym` is a variable in `sys`.
+"""
+function isvariable end
+
+"""
+    variableindex(sys, sym)
+
+Return the index of the given variable `sym` in `sys`, or `nothing` otherwise.
+"""
+function variableindex end
+
+"""
+    isparameter(sys, sym)
+
+Check whether the given `sym` is a parameter in `sys`.
+"""
+function isparameter end
+
+"""
+    parameterindex(sys, sym)
+
+Return the index of the given parameter `sym` in `sys`, or `nothing` otherwise.
+"""
+function parameterindex end
+
+"""
+    isindependent_variable(sys, sym)
+
+Check whether the given `sym` is an independent variable in `sys`.
+"""
+function isindependent_variable end
+
+"""
+    isobserved(sys, sym)
+
+Check whether the given `sym` is an observed value in `sys`.
+"""
+function isobserved end
+
+"""
+    observed(sys, sym)
+
+Return the observed function of the given `sym` in `sys`. The returned function should
+accept a timeseries if `sys` has an independent variable, and return the observed 
+values for the given `sym`.
+"""
+function observed end
+
+"""
+    istimedependent(sys)
+
+Check if `sys` has time as (one of) its independent variables.
+"""
+function istimedependent end
+
+"""
+    constant_structure(sys)
+
+Check if `sys` has a constant structure. Constant structure systems do not change the
+number of variables or parameters over time.
+"""
+function constant_structure end
 
 end
