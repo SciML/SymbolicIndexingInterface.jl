1fe9d57068e61e30dbb999f9979acae1ffcbab28
Aayush Sabharwal <aayush.sabharwal@juliahub.com>
2023-10-20 21:39:15 +0530

    feat: add pre-1.9 compat using Requires

--- src/SymbolicIndexingInterface.jl
+++ src/SymbolicIndexingInterface.jl
@@ -7,5 +7,12 @@ export issymbolic, isvariable, variableindex, isparameter, parameterindex,
     isindependent_variable, isobserved, observed, istimedependent, constant_structure
 include("interface.jl")
 
+@static if !isdefined(Base, :get_extension)
+    using Requires
+    function __init__()
+        @require Symbolics="0c5d862f-8b57-4792-8d23-62f2024744c7" include("../ext/SymbolicIndexingInterfaceSymbolicsExt.jl")
+        @require SymbolicUtils="d1185830-fcd6-423d-90d6-eec64667417b" include("../ext/SymbolicIndexingInterfaceSymbolicUtilsExt.jl")
+    end
+end
 
 end
