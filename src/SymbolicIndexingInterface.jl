5a96b7a94759c57b481dd318b243af1900008d46
Aayush Sabharwal <aayush.sabharwal@juliahub.com>
2023-10-20 20:49:14 +0530

    feat: add `IsSymbolic` trait

--- src/SymbolicIndexingInterface.jl
+++ src/SymbolicIndexingInterface.jl
@@ -1,7 +1,39 @@
 module SymbolicIndexingInterface
 
-export isvariable, variableindex, isparameter, parameterindex, isindependent_variable,
-    isobserved, observed, istimedependent, constant_structure
+export Symbolic, NotSymbolic, issymbolic, isvariable, variableindex, isparameter,
+    parameterindex, isindependent_variable, isobserved, observed, istimedependent,
+    constant_structure
+
+abstract type IsSymbolicTrait end
+
+"""
+    struct Symbolic <: IsSymbolicTrait end
+
+Trait indicating a type is symbolic.
+
+See also: [`NotSymbolic`](@ref), [`issymbolic`](@ref)
+"""
+struct Symbolic <: IsSymbolicTrait end
+
+"""
+    struct NotSymbolic <: IsSymbolicTrait end
+
+Trait indicating a type is not symbolic.
+
+See also: [`Symbolic`](@ref), [`issymbolic`](@ref)
+"""
+struct NotSymbolic <: IsSymbolicTrait end
+
+"""
+    issymbolic(x) = issymbolic(typeof(x))
+    issymbolic(::Type)
+
+Check whether a type implements the [`Symbolic`](@ref) trait or not. Default to
+[`NotSymbolic`](@ref) for all types except `Symbol`.
+"""
+issymbolic(x) = issymbolic(typeof(x))
+issymbolic(::Type) = NotSymbolic()
+issymbolic(::Type{Symbol}) = Symbolic()
 
 """
     isvariable(sys, sym)
