66e59f28dec5f02285105418a1e049fb80a6146f
Aayush Sabharwal <aayush.sabharwal@juliahub.com>
2023-10-23 12:37:24 +0530

    fix: move Requires.jl import

--- src/SymbolicIndexingInterface.jl
+++ src/SymbolicIndexingInterface.jl
@@ -1,5 +1,7 @@
 module SymbolicIndexingInterface
 
+using Requires
+
 export Symbolic, NotSymbolic, issymbolic
 include("trait.jl")
 
@@ -8,7 +10,6 @@ export is_variable, has_static_variable, variable_index, is_parameter, parameter
 include("interface.jl")
 
 @static if !isdefined(Base, :get_extension)
-    using Requires
     function __init__()
         @require Symbolics="0c5d862f-8b57-4792-8d23-62f2024744c7" include("../ext/SymbolicIndexingInterfaceSymbolicsExt.jl")
         @require SymbolicUtils="d1185830-fcd6-423d-90d6-eec64667417b" include("../ext/SymbolicIndexingInterfaceSymbolicUtilsExt.jl")
