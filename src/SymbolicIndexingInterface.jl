064d9f041c329fd7b306f78da826bbb4cdc5131f
Aayush Sabharwal <aayush.sabharwal@juliahub.com>
2023-11-20 13:45:27 +0530

    refactor: remove SymbolicsExt

--- src/SymbolicIndexingInterface.jl
+++ src/SymbolicIndexingInterface.jl
@@ -2,7 +2,7 @@ module SymbolicIndexingInterface
 
 using Requires
 
-export ScalarSymbolic, ArraySymbolic, NotSymbolic, symbolic_type, getname
+export ScalarSymbolic, ArraySymbolic, NotSymbolic, symbolic_type , getname
 include("trait.jl")
 
 export is_variable, variable_index, variable_symbols, is_parameter, parameter_index,
@@ -15,7 +15,6 @@ include("symbol_cache.jl")
 
 @static if !isdefined(Base, :get_extension)
     function __init__()
-        @require Symbolics="0c5d862f-8b57-4792-8d23-62f2024744c7" include("../ext/SymbolicIndexingInterfaceSymbolicsExt.jl")
         @require SymbolicUtils="d1185830-fcd6-423d-90d6-eec64667417b" include("../ext/SymbolicIndexingInterfaceSymbolicUtilsExt.jl")
     end
 end
