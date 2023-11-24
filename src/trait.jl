6a823db4a21ffa94a0b63df57a6eeff1c0cdb3e8
Aayush Sabharwal <aayush.sabharwal@juliahub.com>
2023-11-24 15:31:32 +0530

    docs: improve ArraySymbolic docstring

--- src/trait.jl
+++ src/trait.jl
@@ -12,7 +12,10 @@ struct ScalarSymbolic <: SymbolicTypeTrait end
 """
     struct ArraySymbolic <: SymbolicTypeTrait end
 
-Trait indicating type is a symbolic array or an array of scalar symbolic variables.
+Trait indicating type is a symbolic array. Calling `collect` on a symbolic array must
+return an `AbstractArray` containing `ScalarSymbolic` variables for each element in the
+array, in the same shape as the represented array. For example, if `a` is a symbolic array
+representing a 2x2 matrix, `collect(a)` must return a 2x2 array of scalar symbolic variables.
 
 See also: [`ScalarSymbolic`](@ref), [`NotSymbolic`](@ref), [`symbolic_type`](@ref)
 """
