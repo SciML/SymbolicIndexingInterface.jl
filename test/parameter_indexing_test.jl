1e5450b160c619d9adb259cb4a7307cb36adfa32
Aayush Sabharwal <aayush.sabharwal@juliahub.com>
2023-12-11 17:06:15 +0530

    test: move Symbolics tests to Symbolics downstream

--- test/parameter_indexing_test.jl
+++ test/parameter_indexing_test.jl
@@ -1,5 +1,4 @@
 using SymbolicIndexingInterface
-using Symbolics
 
 struct FakeIntegrator{P}
     p::P
@@ -8,11 +7,10 @@ end
 SymbolicIndexingInterface.symbolic_container(fp::FakeIntegrator) = fp.sys
 SymbolicIndexingInterface.parameter_values(fp::FakeIntegrator) = fp.p
 
-@variables a[1:2] b
-sys = SymbolCache([:x, :y, :z], [a[1], a[2], b], [:t])
-p = [1.0, 2.0, 3.0]
+sys = SymbolCache([:x, :y, :z], [:a, :b], [:t])
+p = [1.0, 2.0]
 fi = FakeIntegrator(copy(p))
-for (i, sym) in [(1, a[1]), (2, a[2]), (3, b), ([1,2], a), ([1, 3], [a[1], b]), ((2, 3), (a[2], b))]
+for (i, sym) in [(1, :a), (2, :b), ([1, 2], [:a, :b]), ((1, 2), (:a, :b))]
     get = getp(sys, sym)
     set! = setp(sys, sym)
     true_value = i isa Tuple ? getindex.((p,), i) : p[i]
--- test/trait_test.jl
+++ test/trait_test.jl
@@ -1,15 +1,6 @@
-using SymbolicUtils
-using Symbolics
 using SymbolicIndexingInterface
 using Test
 
 @test all(symbolic_type.([Int, Float64, String, Bool, UInt, Complex{Float64}]) .==
           (NotSymbolic(),))
-@test all(symbolic_type.([Symbol, SymbolicUtils.BasicSymbolic, Symbolics.Num]) .==
-          (ScalarSymbolic(),))
-@test symbolic_type(Symbolics.Arr) == ArraySymbolic()
-@variables x
-@test symbolic_type(x) == ScalarSymbolic()
-@variables y[1:3]
-@test symbolic_type(y) == ArraySymbolic()
-@test all(symbolic_type.(collect(y)) .== (ScalarSymbolic(),))
+@test symbolic_type(Symbol) == ScalarSymbolic()
