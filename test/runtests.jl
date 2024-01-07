d680331701f027de060583c43bab51f06ab53b44
ArnoStrouwen <arno.strouwen@telenet.be>
2024-01-07 07:25:07 +0100

    fix testset formatting

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,10 +2,10 @@ using SymbolicIndexingInterface
 using SafeTestsets
 using Test
 
-@safetestset "Quality Assurance" @time include("qa.jl")
-@safetestset "Interface test" @time include("example_test.jl")
-@safetestset "Trait test" @time include("trait_test.jl")
-@safetestset "SymbolCache test" @time include("symbol_cache_test.jl")
-@safetestset "Fallback test" @time include("fallback_test.jl")
-@safetestset "Parameter indexing test" @time include("parameter_indexing_test.jl")
-@safetestset "State indexing test" @time include("state_indexing_test.jl")
+@time @safetestset "Quality Assurance" include("qa.jl")
+@time @safetestset "Interface test" include("example_test.jl")
+@time @safetestset "Trait test" include("trait_test.jl")
+@time @safetestset "SymbolCache test" include("symbol_cache_test.jl")
+@time @safetestset "Fallback test" include("fallback_test.jl")
+@time @safetestset "Parameter indexing test" include("parameter_indexing_test.jl")
+@time @safetestset "State indexing test" include("state_indexing_test.jl")
