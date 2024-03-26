0c7810001a0943e58fc99eb9f154189caa47921b
Aayush Sabharwal <aayush.sabharwal@juliahub.com>
2024-03-26 19:39:49 +0530

    refactor: promote types in `remake_buffer` instead of `Union`

--- src/remake.jl
+++ src/remake.jl
@@ -6,8 +6,8 @@ are symbolic variables whose index in the buffer is determined using `sys`. The 
 values in `vals` may not match the types of values stored at the corresponding indexes in
 the buffer, in which case the type of the buffer should be promoted accordingly. In
 general, this method should attempt to preserve the types of values stored in `vals` as
-much as possible. The returned buffer should be of the same type (ignoring type-parameters)
-as `oldbuffer`.
+much as possible. Types can be promoted for type-stability, to maintain performance. The
+returned buffer should be of the same type (ignoring type-parameters) as `oldbuffer`.
 
 This method is already implemented for
 `remake_buffer(sys, oldbuffer::AbstractArray, vals::Dict)` and supports static arrays
@@ -19,11 +19,11 @@ function remake_buffer(sys, oldbuffer::AbstractArray, vals::Dict)
     if ArrayInterface.ismutable(oldbuffer) && !isa(oldbuffer, MArray)
         elT = Union{}
         for val in values(vals)
-            elT = Union{elT, typeof(val)}
+            elT = promote_type(elT, typeof(val))
         end
 
         newbuffer = similar(oldbuffer, elT)
-        setu(sys, collect(keys(vals)))(newbuffer, values(vals))
+        setu(sys, collect(keys(vals)))(newbuffer, elT.(values(vals)))
     else
         mutbuffer = remake_buffer(sys, collect(oldbuffer), vals)
         newbuffer = similar_type(oldbuffer, eltype(mutbuffer))(mutbuffer)
